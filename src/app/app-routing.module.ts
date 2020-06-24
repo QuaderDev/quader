4bf35558113b6902f87fd70b6494af0419a8bfc7
QuaderDev <quader.dev@gmail.com>
2020-06-24 20:48:55 +0300

    about layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-// import { MainAnimationComponent } from './components/main-animation/main-animation.component';
 import { MainWrapperComponent } from './components/main-wrapper/main-wrapper.component';
 import {RiverHouseComponent} from "./components/river-house/river-house.component";
+import {AboutComponent} from "./components/about/about.component";
 
 const routes: Routes = [
   { path: '', component: MainWrapperComponent },
-  {path: 'river-house', component: RiverHouseComponent}
+  {path: 'river-house', component: RiverHouseComponent},
+  {path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
+import {MatSidenavModule} from "@angular/material/sidenav";
+
 import { MainAnimationComponent } from './components/main-animation/main-animation.component';
 import { MainWrapperComponent } from './components/main-wrapper/main-wrapper.component';
 import { MenuComponent } from './components/menu/menu.component';
-
-import {MatSidenavModule} from "@angular/material/sidenav";
 import { RiverHouseComponent } from './components/river-house/river-house.component';
+import { AboutComponent } from "./components/about/about.component";
 
 
 
@@ -20,7 +21,8 @@ import { RiverHouseComponent } from './components/river-house/river-house.compon
     MainAnimationComponent,
     MainWrapperComponent,
     MenuComponent,
-    RiverHouseComponent
+    RiverHouseComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
